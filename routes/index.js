383145dafad95adbd8fe24ec40a979a73496d381
iacoopa <boxfullofnumbers@gmail.com>
2015-07-07 13:49:30 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.cookies.charname) {
-      res.render('sessions', { user: req.cookies.charname});
+      res.render('sessions', { char: req.cookies.charname, user: req.cookies.username});
   } else {
       res.render('sessions');
   }
